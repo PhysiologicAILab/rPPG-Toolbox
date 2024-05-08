c25de494efdc02109ed37d98c9cc608ecde7b089
jnj256 <jnj256@gmail.com>
2024-05-08 15:24:28 +0100

    retraining EfficientPhysFM that gave optimal performance, only with a change of FD from 240 back to 30

--- neural_methods/model/EfficientPhysFM.py
+++ neural_methods/model/EfficientPhysFM.py
@@ -33,8 +33,8 @@ class _MatrixDecompositionBase(nn.Module):
         factor = 8
         # self.R = (BN // factor) if (BN // factor) % 2 == 0 else (BN // factor) + 1
         # # self.R = 2 * frame_depth
-        self.R = 4 * batch_size
-        # self.R = 4
+        # self.R = 4 * batch_size
+        self.R = 4
 
         self.train_steps = model_config["TRAIN_STEPS"]
         self.eval_steps = model_config["EVAL_STEPS"]
@@ -115,9 +115,9 @@ class _MatrixDecompositionBase(nn.Module):
             # N = self.frame_depth  # C * H * W // self.S  # self.frame_depth
             # D = C * self.frame_depth
             # N = H * W
-            D = B * self.frame_depth
-            N = C * H * W
-            B = 1
+            D = C * self.frame_depth
+            N =  H * W
+            # B = 1
             x = x.view(B * self.S, D, N)
 
             # print("---")
@@ -377,8 +377,8 @@ class EfficientPhysFM(nn.Module):
             self.final_dense_1 = nn.Linear(3136, self.nb_dense, bias=True)
         elif img_size == 72:
             # self.final_dense_1 = nn.Linear(16384, self.nb_dense, bias=True)
-            # self.final_dense_1 = nn.Linear(3136, self.nb_dense, bias=True)
-            self.final_dense_1 = nn.Linear(576, self.nb_dense, bias=True)
+            self.final_dense_1 = nn.Linear(3136, self.nb_dense, bias=True)
+            # self.final_dense_1 = nn.Linear(576, self.nb_dense, bias=True)
         elif img_size == 96:
             self.final_dense_1 = nn.Linear(30976, self.nb_dense, bias=True)
         else:
@@ -406,10 +406,8 @@ class EfficientPhysFM(nn.Module):
         d5 = self.dropout_2(d5)
 
         d5 = self.TSM_4(d5)
-        d6 = torch.tanh(self.motion_conv4(d5))
-
-        d6 = self.avg_pooling_3(d6)
-        d6 = self.feature_factorizer(d6)
+        d6 = self.motion_conv4(d5)
+        d6 = torch.tanh(self.feature_factorizer(d6))
 
         d7 = self.avg_pooling_4(d6)
         d8 = self.dropout_3(d7)
@@ -429,7 +427,7 @@ if __name__ == "__main__":
     # default `log_dir` is "runs" - we'll be more specific here
     # writer = SummaryWriter('runs/EfficientPhysFM')
 
-    batch_size = 2
+    batch_size = 8
     frames = 30    #duration*fs
     in_channels = 3
     height = 72
