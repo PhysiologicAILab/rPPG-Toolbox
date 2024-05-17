26b1d9d872d73d35a7f6d015f9de1a636ad0f27d
jnj256 <jnj256@gmail.com>
2024-05-17 17:50:27 +0100

    Training EfficientPhysFM with 90 FD is not optimal, changing back to 20

--- neural_methods/model/EfficientPhysFM.py
+++ neural_methods/model/EfficientPhysFM.py
@@ -34,7 +34,7 @@ class _MatrixDecompositionBase(nn.Module):
         # self.R = (BN // factor) if (BN // factor) % 2 == 0 else (BN // factor) + 1
         # # self.R = 2 * frame_depth
         # self.R = 4 * batch_size
-        # self.R = 4
+        self.R = 4
 
         self.train_steps = model_config["TRAIN_STEPS"]
         self.eval_steps = model_config["EVAL_STEPS"]
@@ -109,7 +109,7 @@ class _MatrixDecompositionBase(nn.Module):
 
         elif self.dim == "2D":      # (B, C, H, W) -> (B * S, D, N)
             BN, C, H, W = x.shape
-            # print("BN, C, H, W", BN, C, H, W)
+            print("BN, C, H, W", BN, C, H, W)
             B = BN // self.frame_depth
             # D = C * H * W // self.S  # self.frame_depth  # C * H * W // self.S
             # N = self.frame_depth  # C * H * W // self.S  # self.frame_depth
@@ -119,14 +119,14 @@ class _MatrixDecompositionBase(nn.Module):
             N = H * W
             # B = 1
             # self.R = min(D, N) // max(C, self.frame_depth)   #since we need to have a rank lower than frame-depth and C
-            self.R = min(D, N) // 16   #since we need to have a rank lower than frame-depth and C
+            # self.R = min(D, N) // 16   #since we need to have a rank lower than frame-depth and C
             x = x.view(B * self.S, D, N)
 
-            # print("---")
-            # print("D", D)
-            # print("R", self.R)
-            # print("N", N)
-            # print("x.shape", x.shape)
+            print("---")
+            print("D", D)
+            print("R", self.R)
+            print("N", N)
+            print("x.shape", x.shape)
 
         else:                       # (B, C, L) -> (B * S, D, N)
             B, C, L = x.shape
