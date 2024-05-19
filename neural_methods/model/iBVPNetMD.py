51166873ccdad9fbc172b92bb5a07743bd9e6ae7
jnj256 <jnj256@gmail.com>
2024-05-19 14:01:06 +0100

    Training v12 of FactorizePhys on PURE and UBFC-rPPG

--- neural_methods/model/iBVPNetMD.py
+++ neural_methods/model/iBVPNetMD.py
@@ -13,7 +13,7 @@ from torch.nn.modules.batchnorm import _BatchNorm
 import numpy as np
 
 # num_filters
-nf = [16, 24, 32, 40, 48]
+nf = [16, 24, 32, 48, 64]
 
 model_config = {
     "INPUT_CHANNELS": 1,
@@ -447,7 +447,7 @@ class encoder_block(nn.Module):
             nn.Dropout3d(p=dropout_rate),
 
             ConvBlock3D(nf[3], nf[3], [5, 1, 1], [1, 1, 1], [2, 0, 0]),
-            ConvBlock3D(nf[3], nf[3], [3, 3, 3], [1, 1, 1], [1, 1, 1]),
+            ConvBlock3D(nf[3], nf[3], [3, 4, 4], [1, 1, 1], [1, 1, 1]),
             ConvBlock3D(nf[3], nf[4], [3, 3, 3], [1, 1, 1], [1, 1, 1])
         )
 
@@ -485,7 +485,7 @@ class decoder_block(nn.Module):
             # nn.ELU(),
             nn.Tanh(),
 
-            nn.Conv3d(nf[1], nf[0], (3, 3, 3), stride=(1, 2, 2), padding=(1, 0, 0)),
+            nn.Conv3d(nf[1], nf[0], (3, 4, 4), stride=(1, 1, 1), padding=(1, 0, 0)),
             # nn.BatchNorm3d(nf[0]),
             # nn.ELU(),
             nn.Tanh(),
@@ -494,7 +494,7 @@ class decoder_block(nn.Module):
             # nn.Dropout3d(p=dropout_rate),
 
             # nn.AvgPool3d((1, 2, 2), stride=(1, 2, 2)),
-            nn.Conv3d(nf[0], 1, (3, 2, 2), (1, 1, 1), (1, 0, 0)),
+            nn.Conv3d(nf[0], 1, (3, 1, 1), (1, 1, 1), (1, 0, 0)),
         )
 
 
