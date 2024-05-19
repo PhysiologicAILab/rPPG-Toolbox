0f72714e2d2abd999ebadc25d7ca6538d4a75631
jnj256 <jnj256@gmail.com>
2024-05-19 17:52:52 +0100

    Training v15 of FactorizePhys on PURE and UBFC-rPPG

--- neural_methods/model/iBVPNetMD.py
+++ neural_methods/model/iBVPNetMD.py
@@ -13,7 +13,7 @@ from torch.nn.modules.batchnorm import _BatchNorm
 import numpy as np
 
 # num_filters
-nf = [16, 24, 32, 40, 48]
+nf = [16, 24, 32, 32, 32]
 
 model_config = {
     "INPUT_CHANNELS": 1,
@@ -92,7 +92,7 @@ class _MatrixDecompositionBase(nn.Module):
             # # From spatial and channel dimension, which are are examples, only 2-4 shall be enough to generate the approximated attention matrix
             D = T
             N = C * H * W // self.S
-            self.R = max(8, min(D, N) // 8)
+            self.R = max(10, min(D, N) // 8)
 
             # D = T * H * W // self.S
             # N = C
@@ -344,7 +344,7 @@ class FeaturesFactorizationModule(nn.Module):
 
         self.device = device
         md_type = model_config["MD_TYPE"]
-        mid_C = in_c // 8
+        mid_C = in_c // 4
         # MD_R = (frames // 4) // 8  # // 4 done by encoder, and //4 for NMF
         MD_R = 8
 
@@ -424,11 +424,11 @@ class encoder_block(nn.Module):
         self.debug = debug
 
         self.encoder = nn.Sequential(
-            ConvBlock3D(inCh, nf[0], [1, 5, 5], [1, 1, 1], [0, 2, 2]),
+            ConvBlock3D(inCh, nf[0], [3, 3, 3], [1, 1, 1], [1, 1, 1]),
             ConvBlock3D(nf[0], nf[0], [3, 3, 3], [1, 2, 2], [1, 1, 1]),
             # nn.MaxPool3d((1, 2, 2), stride=(1, 2, 2)),
 
-            ConvBlock3D(nf[0], nf[0], [5, 1, 1], [1, 1, 1], [2, 0, 0]),
+            # ConvBlock3D(nf[0], nf[0], [5, 1, 1], [1, 1, 1], [2, 0, 0]),
             ConvBlock3D(nf[0], nf[0], [3, 3, 3], [1, 1, 1], [1, 1, 1]),
             ConvBlock3D(nf[0], nf[1], [3, 3, 3], [2, 2, 2], [1, 1, 1]),
             # nn.MaxPool3d((2, 2, 2), stride=(2, 2, 2)),
@@ -439,14 +439,14 @@ class encoder_block(nn.Module):
             # nn.MaxPool3d((1, 2, 2), stride=(1, 2, 2)),
             nn.Dropout3d(p=dropout_rate),
 
-            ConvBlock3D(nf[2], nf[2], [5, 1, 1], [1, 1, 1], [2, 0, 0]),
+            # ConvBlock3D(nf[2], nf[2], [5, 1, 1], [1, 1, 1], [2, 0, 0]),
             ConvBlock3D(nf[2], nf[2], [3, 3, 3], [1, 1, 1], [1, 1, 1]),
             ConvBlock3D(nf[2], nf[3], [3, 3, 3], [2, 1, 1], [1, 1, 1]),
             # nn.MaxPool3d((2, 2, 2), stride=(2, 1, 1)),
             nn.Dropout3d(p=dropout_rate),
 
             ConvBlock3D(nf[3], nf[3], [3, 3, 3], [1, 1, 1], [1, 1, 1]),
-            ConvBlock3D(nf[3], nf[4], [3, 3, 3], [1, 1, 1], [1, 0, 0])
+            ConvBlock3D(nf[3], nf[4], [3, 3, 3], [1, 1, 1], [1, 1, 1])
         )
 
     def forward(self, x):
@@ -466,32 +466,18 @@ class decoder_block(nn.Module):
         # k_t = 3  # 3  # 5   #7
         # pad_t = 1  # 1  # 2   #3
         self.conv_decoder = nn.Sequential(
-            nn.ConvTranspose3d(nf[4], nf[3], (4, 1, 1), (2, 1, 1), (1, 0, 0)),
-            # nn.BatchNorm3d(nf[3]),
-            # nn.ELU(),
-            nn.Tanh(),
-            nn.Conv3d(nf[3], nf[2], (3, 3, 3), stride=(1, 2, 2), padding=(1, 0, 0)),
-            # nn.BatchNorm3d(nf[2]),
-            # nn.ELU(),
+            nn.ConvTranspose3d(nf[4], nf[1], (4, 1, 1), (2, 1, 1), (1, 0, 0)),
             nn.Tanh(),
 
-            # nn.AvgPool3d((1, 2, 2), stride=(1, 2, 2)),
-            # nn.Dropout3d(p=dropout_rate),
-
-            nn.ConvTranspose3d(nf[2], nf[1], (4, 1, 1), (2, 1, 1), (1, 0, 0)),
-            # nn.BatchNorm3d(nf[1]),
-            # nn.ELU(),
+            nn.Conv3d(nf[1], nf[0], (3, 3, 3), stride=(1, 2, 2), padding=(1, 0, 0)),
             nn.Tanh(),
 
-            nn.Conv3d(nf[1], nf[0], (3, 3, 3), stride=(1, 1, 1), padding=(1, 0, 0)),
-            # nn.BatchNorm3d(nf[0]),
-            # nn.ELU(),
+            nn.ConvTranspose3d(nf[0], nf[0], (4, 1, 1), (2, 1, 1), (1, 0, 0)),
             nn.Tanh(),
 
-            # nn.AvgPool3d((1, 2, 2), stride=(1, 2, 2)),
-            # nn.Dropout3d(p=dropout_rate),
+            nn.Conv3d(nf[0], nf[0], (3, 3, 3), stride=(1, 2, 2), padding=(1, 0, 0)),
+            nn.Tanh(),
 
-            # nn.AvgPool3d((1, 2, 2), stride=(1, 2, 2)),
             nn.Conv3d(nf[0], 1, (3, 1, 1), (1, 1, 1), (1, 0, 0)),
         )
 
