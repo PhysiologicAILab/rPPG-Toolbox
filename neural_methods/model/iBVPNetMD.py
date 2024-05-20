781ea5cc6c7ef0bf7dce8a7ff91b4a950c097559
jnj256 <jnj256@gmail.com>
2024-05-20 21:52:41 +0100

    Training v23 of FactorizePhys on iBVP data, to be tested on PURE. Not compressing temporal dimension at all, and reduced the number of filters

--- neural_methods/model/iBVPNetMD.py
+++ neural_methods/model/iBVPNetMD.py
@@ -13,7 +13,7 @@ from torch.nn.modules.batchnorm import _BatchNorm
 import numpy as np
 
 # num_filters
-nf = [8, 24, 32, 48, 64]
+nf = [8, 16, 24, 32, 32]
 
 model_config = {
     "INPUT_CHANNELS": 1,
@@ -312,7 +312,7 @@ class FeaturesFactorizationModule(nn.Module):
 
         self.device = device
         md_type = model_config["MD_TYPE"]
-        mid_C = in_c // 8
+        mid_C = in_c // 4
         # MD_R = (frames // 4) // 8  # // 4 done by encoder, and //4 for NMF
 
         if "nmf" in md_type.lower():
