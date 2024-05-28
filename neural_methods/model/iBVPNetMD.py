2b2f5db586b4336576b93f63084f96aeeda85783
jnj256 <jnj256@gmail.com>
2024-05-28 22:35:59 +0100

    Training for v49 of FactorizePhys, with TanH, MD_S=8, Steps=4, R=8, Batchsize=4, LR=1e-3

--- neural_methods/model/iBVPNetMD.py
+++ neural_methods/model/iBVPNetMD.py
@@ -13,13 +13,13 @@ from torch.nn.modules.batchnorm import _BatchNorm
 import numpy as np
 
 # num_filters
-nf = [4, 4, 4, 4, 7]
+nf = [7, 7, 7, 7, 7]
 
 model_config = {
     "MD_S": 8,
-    "MD_R": 5,
-    "TRAIN_STEPS": 5,
-    "EVAL_STEPS": 5,
+    "MD_R": 8,
+    "TRAIN_STEPS": 4,
+    "EVAL_STEPS": 4,
     "INV_T": 1,
     "ETA": 0.9,
     "RAND_INIT": True,
