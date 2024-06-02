1bf5dab47acbd947fa8983d4044a7bbb1bcab9f4
jnj256 <jnj256@gmail.com>
2024-06-02 22:28:20 +0100

    Training v63_FactorizePhys_NoRes, MD_R=8, MD_S=10, Steps=6

--- neural_methods/model/iBVPNetMD.py
+++ neural_methods/model/iBVPNetMD.py
@@ -16,9 +16,9 @@ import numpy as np
 nf = [8, 16, 16, 16]
 
 model_config = {
-    "MD_S": 4,
-    "MD_R": 1,
-    "MD_STEPS": 4,
+    "MD_S": 10,
+    "MD_R": 8,
+    "MD_STEPS": 6,
     "INV_T": 1,
     "ETA": 0.9,
     "RAND_INIT": True,
