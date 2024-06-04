70910a8a85d5aaa0089f0b0a07097771352705a1
jnj256 <jnj256@gmail.com>
2024-06-04 09:43:35 +0100

    Training v63_FactorizePhys with Res, MD_R=10, MD_S=2, Steps=6

--- neural_methods/model/iBVPNetMD.py
+++ neural_methods/model/iBVPNetMD.py
@@ -16,8 +16,8 @@ import numpy as np
 nf = [8, 16, 16, 16]
 
 model_config = {
-    "MD_R": 1,
-    "MD_S": 4,
+    "MD_R": 10,
+    "MD_S": 2,
     "MD_STEPS": 6,
     "INV_T": 1,
     "ETA": 0.9,
@@ -370,11 +370,13 @@ class FeaturesFactorizationModule(nn.Module):
         att = self.md_block(x)
         dist = torch.dist(x, att)
         att = self.post_conv_block(att)
-        # If residual connection is used, factorization should aim at very low rank approximation to retain only highly important features.
-        x = F.tanh(shortcut + torch.multiply(shortcut, att))
-        # # In this case (no residual connection), factorization should aim at optimal rank approximation, 
-        # # eliminating only some features, while retaining the most
-        # x = F.tanh(torch.multiply(shortcut, att))
+
+        # # If residual connection is used, factorization should aim at very low rank approximation to retain only highly important features.
+        # x = F.tanh(shortcut + torch.multiply(shortcut, att))
+
+        # In this case (no residual connection), factorization should aim at optimal rank approximation, 
+        # eliminating only some features, while retaining the most
+        x = F.tanh(torch.multiply(shortcut, att))
 
         return x, att, dist
 
