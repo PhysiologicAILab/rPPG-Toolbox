9f714d54fe7d56f3810e3c26cf45d07be88b34e1
jnj256 <jnj256@gmail.com>
2024-06-09 15:47:32 +0100

    Training v63_FactorizePhys with Res, no Tanh at merging features, MD_R=8, MD_S=2, Steps=6

--- neural_methods/model/iBVPNetMD.py
+++ neural_methods/model/iBVPNetMD.py
@@ -17,8 +17,8 @@ nf = [8, 16, 16, 16]
 
 model_config = {
     "MD_R": 8,
-    "MD_S": 4,
-    "MD_STEPS": 4,
+    "MD_S": 2,
+    "MD_STEPS": 6,
     "INV_T": 1,
     "ETA": 0.9,
     "RAND_INIT": True,
