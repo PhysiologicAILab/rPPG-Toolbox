b6b4615db9854469880a05183a65423a0e8a6425
jnj256 <jnj256@gmail.com>
2024-06-10 16:53:34 +0100

    Training v63_FactorizePhys with Res, No TanH, FSAM_Norm, VEFM(1+x); individual configs

--- neural_methods/model/iBVPNetMD.py
+++ neural_methods/model/iBVPNetMD.py
@@ -16,9 +16,9 @@ import numpy as np
 nf = [8, 16, 16, 16]
 
 model_config = {
-    "MD_R": 1,
-    "MD_S": 8,
-    "MD_STEPS": 6,
+    "MD_R": 8,
+    "MD_S": 1,
+    "MD_STEPS": 4,
     "INV_T": 1,
     "ETA": 0.9,
     "RAND_INIT": True,
