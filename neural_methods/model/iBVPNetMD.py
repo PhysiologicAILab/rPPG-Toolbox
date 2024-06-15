75207bf5e9eb7e8a509cff84603a11c99da8e83f
jnj256 <jnj256@gmail.com>
2024-06-15 19:16:50 +0100

    Training v63_FactorizePhys Medium with NMF; without Res, MD_Init=ones, Mul, InstanceNorm, R=1, S=8, ST=5

--- neural_methods/model/iBVPNetMD.py
+++ neural_methods/model/iBVPNetMD.py
@@ -19,9 +19,9 @@ nf = [8, 16, 24, 32]
 model_config = {
     "MD_FSAM": True,
     "MD_TYPE": "NMF",
-    "MD_R": 8,
+    "MD_R": 1,
     "MD_S": 8,
-    "MD_STEPS": 6,
+    "MD_STEPS": 5,
     "INV_T": 1,
     "ETA": 0.9,
     "RAND_INIT": True,
