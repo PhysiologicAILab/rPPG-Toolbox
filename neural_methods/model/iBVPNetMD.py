0b413fb7c30d80c350eb7490ab1c78790eb13fd3
jnj256 <jnj256@gmail.com>
2024-06-18 01:42:15 +0100

    v01_FactorizePhys; Mul-NoRes, LR=1e-3, R=1, S=4, ST=4, ELU_BB, Zeros Init, InstanceNorm for input

--- neural_methods/model/iBVPNetMD.py
+++ neural_methods/model/iBVPNetMD.py
@@ -557,14 +557,14 @@ class BVP_Head(nn.Module):
             # # Residual connection: 
             # factorized_embeddings = voxel_embeddings + F.tanh(self.fsam_norm(att_mask))
 
-            # # Multiplication
-            # x = torch.mul(voxel_embeddings - voxel_embeddings.min() + self.bias1, att_mask - att_mask.min() + self.bias1)
-            # factorized_embeddings = self.fsam_norm(x)
-
-            # Multiplication with Residual connection
+            # Multiplication
             x = torch.mul(voxel_embeddings - voxel_embeddings.min() + self.bias1, att_mask - att_mask.min() + self.bias1)
             factorized_embeddings = self.fsam_norm(x)
-            factorized_embeddings = voxel_embeddings + factorized_embeddings
+
+            # # Multiplication with Residual connection
+            # x = torch.mul(voxel_embeddings - voxel_embeddings.min() + self.bias1, att_mask - att_mask.min() + self.bias1)
+            # factorized_embeddings = self.fsam_norm(x)
+            # factorized_embeddings = voxel_embeddings + factorized_embeddings
             
             # # Concatenate
             # factorized_embeddings = torch.cat([voxel_embeddings, self.fsam_norm(x)], dim=1)
