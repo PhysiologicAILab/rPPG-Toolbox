5ec85cb3fd437bb6944be1300e86d71fc50b53d5
jnj256 <jnj256@gmail.com>
2024-06-18 21:25:27 +0100

    v01_FactorizePhys; LR=1e-3, Mul-NoRes, R=1, S=1, ST=6, TanH_II, Rand Init, InstanceNorm for input

--- neural_methods/model/iBVPNetMD.py
+++ neural_methods/model/iBVPNetMD.py
@@ -357,11 +357,11 @@ class ConvBNReLU(nn.Module):
 
         if self.apply_bn:
             if self.dim == "3D":
-                self.bn = nn.BatchNorm3d(out_c)
+                self.bn = nn.InstanceNorm3d(out_c)
             elif self.dim == "2D":
-                self.bn = nn.BatchNorm2d(out_c)
+                self.bn = nn.InstanceNorm2d(out_c)
             else:
-                self.bn = nn.BatchNorm1d(out_c)
+                self.bn = nn.InstanceNorm1d(out_c)
 
     def forward(self, x):
         x = self.conv(x)
@@ -464,9 +464,9 @@ class ConvBlock3D(nn.Module):
         super(ConvBlock3D, self).__init__()
         self.conv_block_3d = nn.Sequential(
             nn.Conv3d(in_channel, out_channel, kernel_size, stride, padding),
-            # nn.Tanh()
-            nn.ELU(inplace=True),
-            nn.BatchNorm3d(out_channel),
+            nn.Tanh(),
+            # nn.ELU(inplace=True),
+            nn.InstanceNorm3d(out_channel),
         )
 
     def forward(self, x):
@@ -517,8 +517,8 @@ class BVP_Head(nn.Module):
         if self.use_fsam:
             inC = nf[3]
             self.fsam = FeaturesFactorizationModule(inC, device, md_config, dim="3D", debug=debug)
-            # self.fsam_norm = nn.InstanceNorm3d(inC)
-            self.fsam_norm = nn.BatchNorm3d(inC)
+            self.fsam_norm = nn.InstanceNorm3d(inC)
+            # self.fsam_norm = nn.BatchNorm3d(inC)
             self.bias1 = nn.Parameter(torch.tensor(1.0), requires_grad=True).to(device)
             # self.bias2 = nn.Parameter(torch.tensor(2.0), requires_grad=False).to(device)
         else:
@@ -527,9 +527,9 @@ class BVP_Head(nn.Module):
         self.conv_decoder = nn.Sequential(
 
             nn.Conv3d(inC, nf[0], (3, 3, 3), stride=(1, 2, 2), padding=(1, 0, 0)),
-            # nn.Tanh(),
-            nn.ELU(inplace=True),
-            nn.BatchNorm3d(nf[0]),
+            nn.Tanh(),
+            # nn.ELU(inplace=True),
+            nn.InstanceNorm3d(nf[0]),
 
             nn.Dropout3d(p=dropout_rate),
 
