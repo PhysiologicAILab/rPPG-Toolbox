199ca978c283441f3b67cb5e949fc3d90e122e2d
jnj256 <jnj256@gmail.com>
2024-06-19 15:48:30 +0100

    v06_FactorizePhys; LR=1e-3, Mul-NoRes, R=1, S=8, ST=5, ELU_BB based on QualPhys behavior, Rand Init

--- neural_methods/model/iBVPNetMD.py
+++ neural_methods/model/iBVPNetMD.py
@@ -186,8 +186,7 @@ class NMF(_MatrixDecompositionBase):
         self.inv_t = 1
 
     def _build_bases(self, B, S, D, R):
-        # bases = torch.rand((B * S, D, R)).to(self.device)
-        bases = torch.ones((B * S, D, R)).to(self.device)
+        bases = torch.rand((B * S, D, R)).to(self.device)
         bases = F.normalize(bases, dim=1)
 
         return bases
@@ -227,8 +226,7 @@ class VQ(_MatrixDecompositionBase):
         self.device = device
 
     def _build_bases(self, B, S, D, R):
-        # bases = torch.randn((B * S, D, R)).to(self.device)
-        bases = torch.ones((B * S, D, R)).to(self.device)
+        bases = torch.randn((B * S, D, R)).to(self.device)
         bases = F.normalize(bases, dim=1)
         return bases
 
