ebac9ad75b84d6ee1e6b4a1a99c029359cb7871f
jnj256 <jnj256@gmail.com>
2024-06-26 14:24:38 +0100

    v06_FactorizePhys; LR=1e-3, Mul-NoRes, Multiple Configs, ELU_II, Ones  Init, Instance Norm, Smoothening temporal dimension, post factorization, Smooth filter = [0.33, 0.66, 1.00, 0.66, 0.33]; (x - x.min())/ x.std()

--- neural_methods/model/iBVPNetMD.py
+++ neural_methods/model/iBVPNetMD.py
@@ -175,15 +175,15 @@ class _MatrixDecompositionBase(nn.Module):
             # print("Intermediate-1 x", x.shape)
 
             sample_1 = x[:, :, 0].unsqueeze(2)
-            # sample_2 = x[:, :, 0].unsqueeze(2)
+            sample_2 = x[:, :, 0].unsqueeze(2)
             sample_3 = x[:, :, -1].unsqueeze(2)
-            # sample_4 = x[:, :, -1].unsqueeze(2)
-            x = torch.cat([sample_1, x, sample_3], dim=2)
-            # x = torch.cat([sample_1, sample_2, x, sample_3, sample_4], dim=2)
+            sample_4 = x[:, :, -1].unsqueeze(2)
+            # x = torch.cat([sample_1, x, sample_3], dim=2)
+            x = torch.cat([sample_1, sample_2, x, sample_3, sample_4], dim=2)
             # gaussian_kernel = [0.25, 0.50, 0.75, 0.50, 0.25]
-            # gaussian_kernel = [0.33, 0.66, 1.00, 0.66, 0.33]
+            gaussian_kernel = [0.33, 0.66, 1.00, 0.66, 0.33]
             # gaussian_kernel = [0.20, 0.80, 1.00, 0.80, 0.20]
-            gaussian_kernel = [1.0, 1.0, 1.0]
+            # gaussian_kernel = [1.0, 1.0, 1.0]
             # gaussian_kernel = [0.66, 1.0, 0.66]
             kernels = torch.FloatTensor([[gaussian_kernel]]).repeat(N, N, 1).to(self.device)
             bias = torch.FloatTensor(torch.zeros(N)).to(self.device)
